0735dccebbe5fb0bfa85d9b7198dbe9dd60565a7
Ryozo Watanabe <clearstor@gmail.com>
2015-05-21 22:47:54 +0900

    h2初期化用SQLの作成

---
+++ src/main/resources/h2-initialize.sql
@@ -0,0 +1,6 @@
+create table base(id bigint auto_increment primary key, value varchar(255) not null);
+
+insert into base (value) values ('value1');
+insert into base (value) values ('value2');
+insert into base (value) values ('value3');
+insert into base (value) values ('value4');
