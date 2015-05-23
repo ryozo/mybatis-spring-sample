b80a52d7233600c13112152ba36efbd6dab26eeb
Ryozo Watanabe <clearstor@gmail.com>
2015-05-23 23:12:27 +0900

    テーブル名の見直し

--- src/main/resources/h2-schema.sql
+++ src/main/resources/h2-schema.sql
@@ -1,2 +1,2 @@
 -- create user table
-create table user(id bigint auto_increment primary key, name varchar(255) not null, age int not null, mail varchar(256));
+create table users(id bigint auto_increment primary key, name varchar(255) not null, age int not null, mail varchar(256));
