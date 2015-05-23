89fe19678435fe94c264ba17484e34796a67f97e
Ryozo Watanabe <clearstor@gmail.com>
2015-05-23 22:51:26 +0900

    ddlとdmlを分離

---
+++ src/main/resources/h2-dataload.sql
@@ -0,0 +1,4 @@
+-- load user table
+insert into user (name, age, mail) values ('taro yamada', 20, 'taro@example.com');
+insert into user (name, age, mail) values ('jiro suzuki', 30, 'jiro@example.com');
+insert into user (name, age, mail) values ('sabro sato', 40, 'sabro@example.com');
--- src/main/resources/h2-initialize.sql
+++
@@ -1,5 +0,0 @@
-create table user(id bigint auto_increment primary key, name varchar(255) not null, age int not null, mail varchar(256));
-
-insert into user (name, age, mail) values ('taro yamada', 20, 'taro@example.com');
-insert into user (name, age, mail) values ('jiro suzuki', 30, 'jiro@example.com');
-insert into user (name, age, mail) values ('sabro sato', 40, 'sabro@example.com');
